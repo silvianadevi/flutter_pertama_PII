3d717e58940c3dd9cb5b86983690acc3596f3cc5
Silviana Devi <silvianadevi1109@gmail.com>
2022-11-04 17:13:15 +0700

    FLUTTER PERTAMA

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -186,6 +186,54 @@ hitungNilai2({mapel1,mapel2}) {
   print (nilaiAkhir);
 }
   
+//oop
+//class
 
+class Kendaraan {
+  String? merk;
+  String? nama;
+  int? kecepatan;
+
+  //construktor
+  Kendaraan({this.merk, this.nama, this.kecepatan});
+
+  //method
+  maju(int tambahKecepatan) {
+    kecepatan = kecepatan! + tambahKecepatan;
+ //   print(kecepatan + tambahKecepatan);
+  }
+}
+
+//inheritance / pewarisan
+class Sedan extends Kendaraan {
+  int? jumlahPintu;
+  int? kecepatanMaksimal;
+
+  Sedan({String? merk, this.jumlahPintu, this.kecepatanMaksimal}):super(merk: merk);
+}
+
+void main() {
+  //instansiasi
+  var k1 = Kendaraan(merk: "BMW", nama: "civic", kecepatan: 20);
+
+  k1.merk = "Toyota";
+//  print(k1.maju(40));
+  print(k1.merk);
+  print(k1.nama);
+  print(k1.kecepatan);
+
+  var k2 = Kendaraan(merk: "Mitsubishi", nama: "colt", kecepatan: 40);
+ var kecepatan = k2.maju(80);
+//  print(kecepatan);
+  print(k2.merk);
+  print(k2.nama);
+  print(k2.kecepatan);
+
+  var s1 = Sedan(jumlahPintu: 4, kecepatanMaksimal: 120, merk: "honda");
+  print("______");
+  print(s1.jumlahPintu);
+  print(s1.kecepatanMaksimal);
+  print(s1.merk);
+}
 
 
